7f9f9db446b1f887aae8ba20f987a5d3e3801452
davide <info.davide99@gmail.com>
2020-08-21 14:19:50 +0200

    Fixed bug in Windows Profiling: 7s => 3s

--- Math/Window.cpp
+++ Math/Window.cpp
@@ -3,17 +3,23 @@
 #include <cmath>
 
 float *Math::Window::get() {
-    if (!Math::Window::winInitialized)
+    if (!Math::Window::winInitialized) {
         for (std::remove_const_t<decltype(Consts::WinSize)> i = 0; i < Consts::WinSize; i++)
             window[i] = (float) (0.5f * (1 - std::cos(2 * M_PI * i / (Consts::WinSize - 1)))); //Hanning
+
+        Math::Window::winInitialized = true;
+    }
 
     return Math::Window::window;
 }
 
 const std::array<float, Consts::FreqBins> &Math::Window::getFreqBins() {
-    if (!Math::Window::binsInitialized)
+    if (!Math::Window::binsInitialized) {
         for (std::remove_const_t<decltype(Consts::FreqBins)> i = 0; i < Consts::FreqBins; i++)
             freqBins[i] = (Consts::SampleRate / 2.0f) * ((float) i / Consts::FreqBins);
+
+        Math::Window::binsInitialized = true;
+    }
 
     return Math::Window::freqBins;
 }
