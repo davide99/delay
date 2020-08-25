849b1ba150e43a63d009c5831d720a7c8bfeea52
Davide <info.davide99@gmail.com>
2020-08-25 15:57:38 +0200

    Better byte swap functions (+ support for MSVC)

--- Math/Integers.cpp
+++ Math/Integers.cpp
@@ -1,12 +1,30 @@
 #include "Integers.h"
-#include <immintrin.h>
 
 std::uint32_t Math::Integers::BSwap(const std::uint32_t &x) {
-    return __bswap_32(x);
+#if defined(_MSC_VER)
+    return _byteswap_ulong((x));
+#elif (__GNUC__ > 4) || (__GNUC__ == 4 && __GNUC_MINOR__ >= 8)
+    return __builtin_bswap32((x));
+#elif defined(__has_builtin) && __has_builtin(__builtin_bswap64)
+    return __builtin_bswap32((x));
+#else
+    return ((( x & 0xff000000u ) >> 24u ) |
+            (( x & 0x00ff0000u ) >> 8u  ) |
+            (( x & 0x0000ff00u ) << 8u  ) |
+            (( x & 0x000000ffu ) << 24u ));
+#endif
 }
 
 std::uint16_t Math::Integers::BSwap(const std::uint16_t &x) {
-    return __bswap_16(x);
+#if defined(_MSC_VER)
+    return _byteswap_ushort((x));
+#elif (__GNUC__ > 4) || (__GNUC__ == 4 && __GNUC_MINOR__ >= 8)
+    return __builtin_bswap16((x));
+#elif defined(__has_builtin) && __has_builtin(__builtin_bswap64)
+    return __builtin_bswap16((x));
+#else
+    return ((( x >> 8u ) & 0xffu ) | (( x & 0xffu ) << 8u ));
+#endif
 }
 
 std::int16_t Math::Integers::BSwap(const std::int16_t &x) {
@@ -16,5 +34,20 @@ std::int16_t Math::Integers::BSwap(const std::int16_t &x) {
 }
 
 std::uint64_t Math::Integers::BSwap(const std::uint64_t &x) {
-    return __bswap_64(x);
+#if defined(_MSC_VER)
+    return _byteswap_uint64((x));
+#elif (__GNUC__ > 4) || (__GNUC__ == 4 && __GNUC_MINOR__ >= 8)
+    return __builtin_bswap64((x));
+#elif defined(__has_builtin) && __has_builtin(__builtin_bswap64)
+    return __builtin_bswap64((x));
+#else
+    return ((( x & 0xff00000000000000ull ) >> 56u ) |
+            (( x & 0x00ff000000000000ull ) >> 40u ) |
+            (( x & 0x0000ff0000000000ull ) >> 24u ) |
+            (( x & 0x000000ff00000000ull ) >> 8u  ) |
+            (( x & 0x00000000ff000000ull ) << 8u  ) |
+            (( x & 0x0000000000ff0000ull ) << 24u ) |
+            (( x & 0x000000000000ff00ull ) << 40u ) |
+            (( x & 0x00000000000000ffull ) << 56u ));
+#endif
 }
