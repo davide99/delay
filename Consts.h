ce8d00bfb3908c13dee085d274a8670a6ff4585e
davide <info.davide99@gmail.com>
2020-08-30 19:45:44 +0200

    Used the std namespace in Consts

--- Consts.h
+++ Consts.h
@@ -6,14 +6,14 @@
 #include <array>
 
 namespace Consts {
-    constexpr uint16_t Channels = 1u;
-    constexpr uint32_t SampleRate = 8000u;
-    constexpr uint16_t BitsPerSample = 16u;
+    constexpr std::uint16_t Channels = 1u;
+    constexpr std::uint32_t SampleRate = 8000u;
+    constexpr std::uint16_t BitsPerSample = 16u;
 
-    constexpr uint16_t WinSize = 512u;
-    constexpr uint16_t Overlap = 256u;
-    constexpr uint16_t StepSize = WinSize - Overlap;
-    constexpr uint16_t FreqBins = WinSize / 2;
+    constexpr std::uint16_t WinSize = 512u;
+    constexpr std::uint16_t Overlap = 256u;
+    constexpr std::uint16_t StepSize = WinSize - Overlap;
+    constexpr std::uint16_t FreqBins = WinSize / 2;
 
     namespace Window {
         constexpr std::uint16_t MelStart = 250u;
